397175c7f1e734b13def4dca50c55416ab3d97d9
AnthonyCointre <8732@holbertonstudents.com>
2024-06-11 10:25:44 +0200

    modification test_review.py

---
+++ tests/test_review.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from models.base_model import BaseModel
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """
+    Classe de tests pour la classe Review.
+    Utilise le framework de tests unitaires unittest.
+    """
+
+    def setUp(self):
+        """
+        Configure une instance de Review avant chaque test.
+        """
+
+        self.review = Review(review_id="review_1", user="user_1", place="place_1")
+
+    def test_review_initialization(self):
+        """
+        Teste si l'instance de Review est correctement initialisée.
+        """
+
+        self.assertEqual(self.review.review_id, "review_1")
+        self.assertEqual(self.review.user, "user_1")
+        self.assertEqual(self.review.place, "place_1")
+
+    def test_repr(self):
+        """
+        Teste la méthode __repr__ pour la représentation en chaîne de caractères de l'avis.
+        """
+
+        expected_repr = "Review(review_1, user_1, place_1)"
+        self.assertEqual(repr(self.review), expected_repr)
+
+
+if __name__ == '__main__':
+    unittest.main()
