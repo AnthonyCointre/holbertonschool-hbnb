948f95c6d8d2687e092da673391136f3d238a1b4
AnthonyCointre <8732@holbertonstudents.com>
2024-06-11 10:29:22 +0200

    modification test_amenty.py

---
+++ tests/test_amenity.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """
+    Classe de tests pour la classe Amenity.
+    Utilise le framework de tests unitaires unittest.
+    """
+
+    def setUp(self):
+        """
+        Configure une instance de Amenity avant chaque test.
+        """
+
+        self.amenity = Amenity(amenity_id="1", amenity_name="Pool")
+
+    def test_amenity_initialization(self):
+        """
+        Teste si l'instance de Amenity est correctement initialisée.
+        """
+
+        self.assertEqual(self.amenity.id, "1")
+        self.assertEqual(self.amenity.name, "Pool")
+
+    def test_repr(self):
+        """
+        Teste la méthode __repr__ pour la représentation en chaîne de caractères de l'agrément.
+        """
+
+        expected_repr = "amenity 1 Pool"
+        self.assertEqual(repr(self.amenity), expected_repr)
+
+
+if __name__ == '__main__':
+    unittest.main()
