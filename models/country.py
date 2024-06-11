b101cf8cfc60a852847cbc208e3c1991647d31e4
AnthonyCointre <8732@holbertonstudents.com>
2024-06-11 10:36:41 +0200

    modification country.py

--- models/country.py
+++ models/country.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
+
 from models.base_model import BaseModel
 
 
-class country(BaseModel):
-    """class that inherits from BaseModel"""
+class Country(BaseModel):
+    """
+    Classe Country qui hérite de BaseModel.
+    """
+
     def __init__(self, country_id="", country_name=""):
+        """
+        Initialise une nouvelle instance de Country.
+        """
+
+        super().__init__(country_id)
         self.id = country_id
         self.name = country_name
-        
-    def add(self):
-        """Add a country"""
-        self.country.append(country)
+        self.countries = []
+
+    def add(self, country):
+        """
+        Ajoute un pays à la liste des pays.
+        """
+
+        self.countries.append(country)
+
+    def __repr__(self):
+        """
+        Retourne une représentation sous forme de chaîne de caractères du pays.
+        """
+
+        return f"country {self.id} {self.name}"
