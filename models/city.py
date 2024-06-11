e84797c27875228eb99eddf327e9848ab279752a
AnthonyCointre <8732@holbertonstudents.com>
2024-06-11 10:33:05 +0200

    modification city.py

--- models/city.py
+++ models/city.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python3
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """class that inherits from BaseModel"""
+    """
+    Classe City qui hérite de BaseModel.
+    """
 
     def __init__(self, city_id="", city_name="", city_country=""):
+        """
+        Initialise une nouvelle instance de City.
+        """
+
+        super().__init__(city_id)
         self.id = city_id
         self.name = city_name
         self.country = city_country
 
     def __repr__(self):
-        """String representation of the city"""
+        """
+        Retourne une représentation sous forme de chaîne de caractères de la ville.
+        """
+
         return f"city {self.id} {self.name} {self.country}"
 
 
 class CityCollection:
-    """a collection of cities"""
+    """
+    Classe CityCollection pour gérer une collection de villes.
+    """
 
     def __init__(self):
+        """
+        Initialise une nouvelle instance de CityCollection.
+        """
+
         self.cities = []
 
     def add(self, city):
-        """Add a city to a collection of cities"""
+        """
+        Ajoute une ville à la collection.
+        """
+
         self.cities.append(city)
