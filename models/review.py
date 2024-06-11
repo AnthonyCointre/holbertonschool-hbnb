2a2b18b85c732c8774af7669e1d12ade31a03abc
AnthonyCointre <8732@holbertonstudents.com>
2024-06-11 10:25:56 +0200

    modification review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """
+    Classe Review qui hérite de BaseModel.
+    """
+
     def __init__(self, review_id, user, place):
+        """
+        Initialise une nouvelle instance de Review.
+        """
+
+        super().__init__(review_id)
         self.review_id = review_id
         self.user = user
         self.place = place
 
     def __repr__(self):
-        return  f"Review({self.review_id}, {self.user}, {self.place})"
+        """
+        Retourne une représentation sous forme de chaîne de caractères de l'avis.
+        """
+
+        return f"Review({self.review_id}, {self.user}, {self.place})"
