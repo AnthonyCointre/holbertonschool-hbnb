58cf0994b783453bb9da18f8f1913445bf4b3276
AnthonyCointre <8732@holbertonstudents.com>
2024-06-13 11:38:52 +0200

    modification app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,2 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-import os
-import json
-from routes.user_routes import user_blueprint, load_users_from_file
-
-app = Flask(__name__)
-
-app.register_blueprint(user_blueprint, url_prefix='/users')
-
-load_users_from_file()
-
-if __name__ == '__main__':
-    app.run(debug=True)
